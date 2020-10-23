bc08d068b7865c3114c1fe1215498e18e067d3cc
Cassie Schultz <CassieSchultz35@gmail.com>
2020-10-22 21:52:36 -0500

    Added a Start to the File Retriever. I added what I believe is code that 'starts the conversation' with the server.  Wanted to get this set up so I could gain a better basic understanding of how the dataPackets work.  I created a datapacket and as of now it is just sending an empty buf to localhost.

--- src/segmentedfilesystem/FileRetriever.java
+++ src/segmentedfilesystem/FileRetriever.java
@@ -1,13 +1,36 @@
 package segmentedfilesystem;
+import java.io.IOException; 
+import java.net.DatagramPacket; 
+import java.net.DatagramSocket; 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
 
-public class FileRetriever {
 
-	public FileRetriever(String server, int port) {
-        // Save the server and port for use in `downloadFiles()`
-        //...
-	}
+public class FileRetriever  {
+        String server;
+        int port;
+        //Constructor 
+	public FileRetriever(String server, int port) throws UnknownHostException {
+                this.server=server;
+                this.port=port;                
+        }
+        
+        //method to start conversation with server
+        public static void start(int port, String server ) throws IOException{
+                InetAddress inetAddress = InetAddress.getByName(server);
+                DatagramSocket socket = new DatagramSocket(port);
+                byte[] buf = new byte[0]; 
+                DatagramPacket dp = new DatagramPacket(buf, buf.length, inetAddress, port);
+                socket.send(dp);
+                
+        }
+        // method to retrive packets 
+	public void downloadFiles() throws IOException {
+                start(this.port, this.server);
 
-	public void downloadFiles() {
+
+
         // Do all the heavy lifting here.
         // This should
         //   * Connect to the server
@@ -20,6 +43,9 @@ public class FileRetriever {
         // PacketManager.allPacketsReceived() that you could
         // call for that, but there are a bunch of possible
         // ways.
-	}
+        }
+       
+      
 
 }
+
