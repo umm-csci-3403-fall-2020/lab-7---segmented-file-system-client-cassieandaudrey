7c4b47b1b1a0a6177c20119dd230f23f9c60190a
schu4276 <69864668+schu4276@users.noreply.github.com>
2020-10-24 11:17:40 -0500

    Update Main.java

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) throws IOException{
         String server = "localhost";
        
-        int port = 6013;
+        int port = 8080;
         
         if (args.length >= 1) {
             server = args[0];
